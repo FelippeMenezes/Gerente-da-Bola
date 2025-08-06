ff153b508eaffad718b5b2ca57ea5022def52b69
Felippe Santana <42714571+FelippeMenezes@users.noreply.github.com>
2025-08-06 13:03:37 -0300

    Create Team, Player and Match Models with migrate and models references

---
+++ app/models/team.rb
@@ -0,0 +1,8 @@
+class Team < ApplicationRecord
+  belongs_to :user, optional: true
+  has_many :players, dependent: :destroy
+  has_many :home_matches, class_name: 'Match', foreign_key: 'home_team_id', dependent: :destroy
+  has_many :away_matches, class_name: 'Match', foreign_key: 'away_team_id', dependent: :destroy
+
+  validates :name, presence: true
+end
